3f6d7861e27dc481d11e9ddb4b5b0c828a7d6032
Andrei Turgo <abturgo@gmail.com>
2025-03-03 15:05:17 +0800

    deployment

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import numpy as np
 import google.generativeai as genai
 import requests
 
-API_URL = "http://127.0.0.1:5000" 
+API_URL = "https://project-a2bt.onrender.com" 
 
 if st.button("Check API"):
     response = requests.get(f"{API_URL}/")
