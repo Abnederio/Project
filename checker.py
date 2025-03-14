8b33013bd81937d2ffc66ab0344e42236701415b
Andrei Turgo <abturgo@gmail.com>
2025-03-14 19:50:12 +0800

    google drive image

--- checker.py
+++ checker.py
@@ -3,7 +3,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 SHEET_ID = "1NCHaEsTIvYUSUgc2VHheP1qMF9nIWW3my5T6NpoNZOk"
 SCOPE = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-CREDS_FILE = "civic-pulsar-453709-f7-0132ae491663.json"
+CREDS_FILE = "civic-pulsar-453709-f7-10c1906e9ce5.json"
 
 creds = ServiceAccountCredentials.from_json_keyfile_name(CREDS_FILE, SCOPE)
 client = gspread.authorize(creds)
